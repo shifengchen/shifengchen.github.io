1501e3e48ab5b22e404556f3b0eb667efbc0561f
sf <shifeng_c@yahoo.com>
2017-04-02 11:31:31 +0800

    canvas responsive

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// $(document).ready(function () {
+//   function resize(){
+//     $("#webgl").outerWidth($(window).width()-$("#webgl").offset().top- Math.abs($("#canvas").outerWidth(true) - $("#canvas").outerWidth()));
+//   }
+//   $(document).ready(function(){
+//     resize();
+//     $(window).on("resize", function(){
+//         resize();
+//     });
+//   });
+// });
