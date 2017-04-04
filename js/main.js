fa0ef6ad1d6761cfeef0d6ad067de089a6dc6718
sf <shifeng_c@yahoo.com>
2017-04-04 21:34:50 +0800

    Add one more background-image,Edit navbar of copy,And add tool module

--- js/main.js
+++ js/main.js
@@ -1,11 +1,35 @@
 // $(document).ready(function () {
-//   function resize(){
-//     $("#webgl").outerWidth($(window).width()-$("#webgl").offset().top- Math.abs($("#canvas").outerWidth(true) - $("#canvas").outerWidth()));
-//   }
-//   $(document).ready(function(){
-//     resize();
-//     $(window).on("resize", function(){
-//         resize();
-//     });
+//   $(".navbar-nav li").click(function () {
+//     $(this).addClass("active");
 //   });
+// });
+
+// $(document).ready(function () {
+//   $(".navbar-nav li").each(function() {
+//     $(this).removeClass();
+//   })
+//   $(".navbar-nav li").click(function () {
+//     $(this).addClass("active");
+//   });
+// })
+
+// $(document).ready(function () {
+//   $(".navbar-collapse").find("li").each(function(){
+//     var a = $(this).find("a:first")[0];
+//     console.log(a);
+//     console.log(location.pathname);
+//     if ($(a).attr("href") === "#"){
+//         $(this).removeClass("active");
+//     }else{
+//         $(this).addClass("active");
+//     }
+//   });
+// });
+
+// $(function() {
+// 	$('.navbar-nav li a').filter(function(){
+// 		return this.href==location.href}).parents("li").addClass('active').siblings().removeClass('active')
+// 	$('.navbar-nav li a').click(function(){
+// 		$(this).parent().parents("li").addClass('active').siblings().removeClass('active')
+// 	})
 // });
